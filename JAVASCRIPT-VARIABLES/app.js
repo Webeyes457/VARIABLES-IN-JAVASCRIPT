c7dde83c3c2a1b272ef98f5b613b6204bf65b2eb
Muhammad Murtaza <121543125+Webeyes457@users.noreply.github.com>
2023-02-13 15:17:48 +0500

    Add files via upload

---
+++ JAVASCRIPT-VARIABLES/app.js
@@ -0,0 +1,21 @@
+//  CHAPTER NO 2  
+
+
+var int = "MY NAME IS MURTAZA  \n MY FATHER NAME IS SALEEM \n I AM 19 YEAR OLD \n MY USERNAME IS MURTAZASALEEM457 \n MY  PASSWORD IS MURTAZA123 \n I AM LEARING JAVACRIPT \n THIS IS MY FIRST CODE HELLO WORL"
+var name = " MY NAME IS MURTAZA"
+var Fname ="  MY FATHER NAME IS SALEEM"
+var age = "I AM 19 YEAR OLD"
+var username = " MY USERNAME IS MURTAZASALEEM457"
+var password = " MY  PASSWORD IS MURTAZA123"
+var title = "I AM LEARING JAVACRIPT"
+var message = " THIS IS MY FIRST CODE HELLO WORLD"
+
+alert(int)
+alert(name)
+alert(Fname)
+alert(age)
+alert(username)
+alert(password)
+alert(title)
+alert(message)
+alert(thing)
